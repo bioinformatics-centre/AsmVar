1ede17f543f3b78068994c6356968dbfba4162d0
Shujia Huang <huangshujia9@gmail.com>
2014-05-28 19:48:04 +0200

    mv src into the directory of AsmvarVarScore

---
+++ AsmvarVarScore/src/model/VariantDatum.py
@@ -0,0 +1,21 @@
+"""
+==============================================
+Same with GATK
+==============================================
+
+Author : Shujia Huang
+Date   : 2014-05-20 17:49:27
+"""
+
+class VariantDatum :
+
+    def __init__ ( self ) :
+        self.annotations        = []
+        self.lod                = None 
+        self.prior              = 2.0
+        self.atTrainingSite     = False
+        self.atAntiTrainingSite = False
+        self.failingSTDThreshold = False
+        self.worstAnnotation     = None
+        self.variantContext      = [] 
+
---
+++ AsmvarVarScore/src/model/VariantRecalibratorArgumentCollection.py
@@ -0,0 +1,19 @@
+"""
+====================================
+====================================
+Author : Shujia Huang
+Date   : 2014-05-21 18:03:28
+
+"""
+class VariantRecalibratorArgumentCollection :
+
+    def __init__ (self) :
+        self.NITER = 150
+        self.NINIT = 100
+        self.STD_THRESHOLD         = 10.0
+        self.MIN_NUM_BAD_VARIANTS  = 1000
+        self.MAX_NUM_TRAINING_DATA = 2500000
+        self.BAD_LOD_CUTOFF        = -5.0
+        self.MAX_GAUSSIANS         = 8
+        self.MAX_GAUSSIANS_FOR_NEGATIVE_MODEL = 4
+
