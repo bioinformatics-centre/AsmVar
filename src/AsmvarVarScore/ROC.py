c2c69760ec28325378eb993ac58aceeb9abea7d1
ShujiaHuang <huangshujia9@gmail.com>
2014-05-29 17:01:30 +0200

    make ROC.py fit the new VCF format

--- src/AsmvarVarScore/ROC.py
+++ src/AsmvarVarScore/ROC.py
@@ -17,8 +17,6 @@ from sklearn.metrics import roc_curve, auc
 
 def main ( opt ) :
 
-    sampleID = opt.sample
-
     if opt.vcfInfile[-3:] == '.gz' :
         I = os.popen( 'gzip -dc %s' % opt.vcfInfile )
     else :
@@ -30,33 +28,14 @@ def main ( opt ) :
 
         for line in lines :
 
-            line = line.strip('\n')
-            col  = line.split()
-            if re.search(r'^#CHROM', line) :
-                if len(col) < 10 :
-                    print >> sys.stderr, '# [ERROR] The input vcf file (%s) does not contain the "Sample" fields!\n' % vcfInfile
-                    sys.exit(1)
-                sam2col = { sam:i+9 for i,sam in enumerate(col[9:]) }
-                if sampleID not in sam2col :
-                    print >> sys.stderr, '# [ERROR] The input sample id (%s) is not match in Vcf file(%s)\n' % ( sampleID, vcfInfile)
-                    sys.exit(1)
             if re.search(r'^#', line) : continue
-            idx  = sam2col[sampleID]
-            
+            col  = line.strip('\n').split()
             flag = re.search(r';Novel=\(([^\|]+)\|', col[7]).group(1)
             if flag != 'Novel' and flag != 'ESame' and flag != 'Same' : continue
-
-            fmat = { k:i for i,k in enumerate( col[8].split(':') ) } # Get Format
-            for type in ['VQ'] :
-                if type not in fmat :
-                    print >> sys.stderr, '# [ERROR] The "Format" fields did not contian %s in VCF %s' %( 'AA', opt.vcfInfile )
-                    sys.exit(1)
-
-            vq = string.atoi(col[idx].split(':')[ fmat['VQ'] ])
+            vq = string.atoi( re.search(r';VQ=([^;]+)', col[7]).group(1) )
             v  = 0
             if 'Same' in flag : v = 1
             data.append([vq, v])
-                
     I.close()
     DrawFig( opt.figure, np.array(data) )
 
@@ -75,7 +54,7 @@ def DrawFig ( figPrefix, data ) :
 
     #plt.show()
     fig.savefig(figPrefix + '.png')
-    #fig.savefig(figPrefix + '.pdf')
+    fig.savefig(figPrefix + '.pdf')
 
     for i in range( len(thresholds) ) :
         print '%f\t%f\t%f' % (thresholds[i], fpr[i], tpr[i])
@@ -87,11 +66,10 @@ if __name__ == '__main__' :
     optp  = optparse.OptionParser(usage=usage)
     optp.add_option('-i', '--vcf' , dest='vcfInfile', metavar='VCF', help='Input vcf file.'      , default=[] )
     optp.add_option('-f', '--fig' , dest='figure'   , metavar='FIG', help='Figure output prefix.', default='test' )
-    optp.add_option('-s', '--smp' , dest='sample'   , metavar='SMP', help='Sample ID.'           , default=[] )
 
     opt, _ = optp.parse_args()
     print >> sys.stderr, '# [INFO] Paraeters: python' , sys.argv[0], '\n\t-i', opt.vcfInfile, \
-          '\n\t-s', opt.sample, '\n\t-f', opt.figure, '\n'
+         '\n\t-f', opt.figure, '\n'
     main( opt )
     print >> sys.stderr, '*********************** ALL DONE ***********************\n'
 
