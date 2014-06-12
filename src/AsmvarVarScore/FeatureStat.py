fca79a58b5cb9a51b47d17c6d4743dde9321bfab
ShujiaHuang <huangshujia9@gmail.com>
2014-06-12 21:54:57 +0200

    Improve the figure

--- src/AsmvarVarScore/FeatureStat.py
+++ src/AsmvarVarScore/FeatureStat.py
@@ -30,19 +30,18 @@ def DrawFig( figureFile, distance, nr, aa, bb ) :
     plt.subplot(222)
     plt.title('N Ratio', fontsize=16)
     plt.plot(nr[:,0], nr[:,2]/np.sum(nr[:,1]), 'yo-' )
-    plt.axis([0,5,0.0,1.0])
     plt.xlabel('N Ratio of varints\' regions(>=%)', fontsize=16)
     plt.ylabel('% of Accumulate', fontsize=16)
 
     plt.subplot(223)
     plt.plot(aa[:,0], aa[:,2]/np.sum(aa[:,1]), 'mo-' )
-    plt.axis([0,100,0.0,1.0])
+    #plt.axis([0,100,0.0,1.0])
     plt.xlabel('Perfect Depth(<=)', fontsize=12)
     plt.ylabel('% of Accumulate', fontsize=16)
 
     plt.subplot(224)
     plt.plot(bb[:,0], bb[:,2]/np.sum(bb[:,1]), 'ko-' )
-    plt.axis([0,100,0.0,1.0])
+    #plt.axis([0,100,0.0,1.0])
     plt.xlabel('Both ImPerfect Depth(<=)', fontsize=12)
     plt.ylabel('% of Accumulate', fontsize=16)
 
@@ -90,7 +89,6 @@ def LoadFaLen ( faLenLstFile ) :
     I.close()
     return data
 
-
 def main ( argv ) :
 
     qFaLen    = LoadFaLen( argv[1] )
@@ -101,8 +99,7 @@ def main ( argv ) :
     else :
         I = open ( argv[0] )
 
-    data, distance, nr, aa, bb = [],{},{},{},{}
-    s    = set()
+    s, annotations, mark = set(), [], []
     print '#Chr\tPosition\tDistance\tLeftIden\tRightIden\tAveIden\tN-Ratio\tAA'
     while 1 : # VCF format
 
@@ -119,58 +116,84 @@ def main ( argv ) :
             s.add(key)
 
             #if re.search(r'^PASS', col[6] ) : continue
-            if not re.search(r'^PASS', col[6] ) : continue
             #if not re.search(r'_TRAIN_SITE', col[7]) : continue
+            #if not re.search(r'^PASS', col[6] ) : continue
 
             fmat = { k:i for i,k in enumerate( col[8].split(':') ) }
             if 'VS' not in fmat or 'QR' not in fmat: continue
+            if len(annotations) == 0 : annotations = [ [] for _ in col[9:] ]
 
-            annotations = []
+            vcfinfo = { d.split('=')[0] : d.split('=')[1] for d in col[7].split(';') if len(d.split('=')) == 2 }
+            vq      = string.atof( vcfinfo['VQ'] )
+            if ('POSITIVE_TRAIN_SITE' in col[7]) and ('NEGATIVE_TRAIN_SITE' in col[7]) :
+                mark.append( [3, vq] )
+            elif 'POSITIVE_TRAIN_SITE' in col[7] : 
+                mark.append( [1, vq] )
+            elif 'NEGATIVE_TRAIN_SITE' in col[7] : 
+                mark.append( [2, vq] )
+            else :
+                mark.append( [0, vq] )
+
             for i, sample in enumerate ( col[9:] ) :
                 sampleId = col2sam[9+i]
-                if len( sample.split(':')[fmat['AA']].split(',') ) != 4 :
-                    print >> sys.stderr,'[WARNING] %s\n%s' % (line, sample.split(':')[fmat['AA']])
-                    continue
                 qr = sample.split(':')[fmat['QR']].split(',')[-1]
-                if qr == '.' : continue
+                if qr == '.' : 
+                    annotations[i].append( [0, 0, 0, 0, 0, 0] )
+                    continue
 
                 qId, qSta, qEnd = qr.split('-')
                 qSta = string.atoi(qSta)
                 qEnd = string.atoi(qEnd)
 
-                if sampleId not in qFaLen           : raise ValueError ('[ERROR] The sample name $s(in vcf) is not in the name of Fa list.' % sampleId )
-                if      qId not in qFaLen[sampleId] : raise ValueError ('[ERROR]', qId, 'is not been found in file', opt.qFalen, '\n' )
+                if sampleId not in qFaLen : raise ValueError ('[ERROR] The sample name $s(in vcf) is not in the name of Fa list.' % sampleId )
+                if qId not in qFaLen[sampleId] : raise ValueError ('[ERROR]', qId, 'is not been found in file', opt.qFalen, '\n' )
                 qSta= int( qSta * 100 / qFaLen[sampleId][qId] + 0.5 )
                 qEnd= int( qEnd * 100 / qFaLen[sampleId][qId] + 0.5 )
                 if qSta > 100 or qEnd > 100 : raise ValueError ('[ERROR] Query size Overflow! sample : %s; scaffold : %s' % (sampleId, qId) )
-
-                leg  = qSta
+                leg = qSta
                 if 100 - qEnd < qSta : leg = qEnd
                 nn  = string.atof(sample.split(':')[fmat['FN']])
-                n   = int(1000 * nn + 0.5) / 10.0
+                n   = round( 1000 * nn ) / 10.0
                 alt = string.atoi( sample.split(':')[fmat['AA']].split(',')[1] ) # Alternate perfect
                 bot = string.atoi( sample.split(':')[fmat['AA']].split(',')[3] ) # Both imperfect
                 pro = string.atoi( sample.split(':')[fmat['RP']].split(',')[0] ) # Proper Pair
                 ipr = string.atoi( sample.split(':')[fmat['RP']].split(',')[1] ) # ImProper Pair
-                annotations.append( [leg, n , alt, bot, pro, ipr] )
-
-            leg, n, alt, bot, pro, ipr = np.median( annotations, axis = 0 )
-            if leg       not in distance : distance[leg]       = [0,0] 
-            if n         not in nr       : nr[n]               = [0,0]
-            if alt       not in aa       : aa[alt]             = [0,0]
-            if bot       not in bb       : bb[bot]             = [0,0]
-            distance[leg][0]       += 1
-            nr[n][0]               += 1
-            aa[alt][0]             += 1
-            bb[bot][0]             += 1
- 
-            data.append([leg, alt, pro, ipr, n, bot])
+                annotations[i].append( [leg, n, alt, bot, pro, ipr] )
     I.close()
+    print >> sys.stderr, '# Number of Positions: %d' % len( mark )
+    if len( mark ) != len( annotations[0] ) : raise ValueError ('[ERROR] The size is not match!')
+    annotations = np.array( annotations );
+
+    sampleNum = len( annotations )
+    for i in range( sampleNum ) : 
+        if np.sum(annotations[i]) == 0: continue
+        mean = np.array( [ d for d in annotations[i] if np.sum(d) > 0 ] ).mean(axis=0)
+        std  = np.array( [ d for d in annotations[i] if np.sum(d) > 0 ] ).std (axis=0)
+        annotations[i] = np.array( (annotations[i] - mean)/std )  # Normalization Per sample
+
+    data, distance, nr, aa, bb = [],{},{},{},{}
+    for i in range( len(annotations[0]) ) : 
+
+        #if mark[i][0] != 1 and mark[i][0] != 3: continue # Positive
+        if mark[i][0] != 2 and mark[i][0] != 3: continue # Nagative
+
+        anno = np.array( [ annotations[s][i] for s in range( sampleNum ) if len(annotations[s][i][annotations[s][i]!=0]) > 0 ] ) # each person in the same position
+        if len( anno ) == 0 : continue
+        leg, n, alt, bot, _1, _2 = np.median( anno, axis=0 )
+
+        if leg not in distance : distance[leg] = [0,0]
+        if n   not in nr       : nr[n]         = [0,0]
+        if alt not in aa       : aa[alt]       = [0,0]
+        if bot not in bb       : bb[bot]       = [0,0]
+        distance[leg][0]       += 1
+        nr[n][0]               += 1
+        aa[alt][0]             += 1
+        bb[bot][0]             += 1
+        data.append([leg, alt, n, bot])
 
     data = np.array(data)
     print >> sys.stderr, '\nPosition\tALTernatePerfect\tLeftIdentity\tRightIdentity\tAveIden\tNRatio\tBothImperfect'
-    print >> sys.stderr, 'Means: ', data.mean(axis=0), '\nstd  : ', data.std(axis=0), '\nMedian: ', np.median( data, axis=0 ) 
-    print >> sys.stderr, '25 Percentile:', np.percentile(data, 25,axis=0), '\n50 Percentile:', np.percentile(data, 50,axis=0), '\n75 Percentile:', np.percentile(data, 75,axis=0)
+    print >> sys.stderr, 'Means:',data.mean(axis=0),'\nstd:',data.std(axis=0),'\nMedian:',np.median(data,axis=0) 
 
     DrawFig( figPrefix, \
              np.array (Accum( distance )), \
@@ -178,9 +201,7 @@ def main ( argv ) :
              np.array (Accum( aa       )), \
              np.array (Accum( bb       )) )
 
-
 if __name__ == '__main__' :
 
     main(sys.argv[1:])
-
 
