5df33ed38c4060b7f2665675e8331880b84c38f7
ShujiaHuang <huangshujia9@gmail.com>
2014-06-23 10:06:16 +0200

    output summary

--- src/AsmvarVarScore/ASV_VarScore.py
+++ src/AsmvarVarScore/ASV_VarScore.py
@@ -65,7 +65,7 @@ def main ( opt ) :
     ## Output Summary
     print >> sys.stderr, '\n[Summmary] Here is the summary information:\n'
     for k, v in sorted (good.items(), key = lambda k:k[0]) : 
-        print >> sys.stderr, '  ** Good Site score >= %d: %d\t%0.2f' % ( k, v, v * 100 / tot)
+        print >> sys.stderr, '  ** Variant Site score >= %d: %d\t%0.2f' % ( k, v, v * 100 / tot)
     for k, v in sorted( culprit.items(), key = lambda k:k[0] ) :
         print >> sys.stderr, '  ** Culprit by %s: %d\t%.2f' % ( k, v, v * 100.0 / tot )
 
