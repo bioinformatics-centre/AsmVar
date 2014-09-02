0e559dd7e7a2082b4eb9f5167e273b64534366f2
Shujia Huang <huangshujia9@gmail.com>
2014-09-02 15:37:17 +0800

    Test change

--- demo/Test.sh
+++ demo/Test.sh
@@ -1,1 +1,2 @@
-time ../src/AsmvarDetect/out3 -i align.maf -t b36_1_1000000_3000000.fa -q query.fa -s 1025 -o t4.1025 >t.age 2> log
+time ../src/AsmvarDetect/ASV_VariantDetector -i align.maf -t b36_1_1000000_3000000.fa -q query.fa -s 1025 -o t4.1025 > t.age
+#time ../src/AsmvarDetect/out3 -i align.maf -t b36_1_1000000_3000000.fa -q query.fa -s 1025 -o t4.1025 >t.age 2> log
