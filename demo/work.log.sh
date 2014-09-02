8bf328fa95e1e479ba7a1ddd53ccefcc44ee15ac
Shujia Huang <huangshujia9@gmail.com>
2014-09-02 09:51:29 +0200

    update the Test alignment result

--- demo/work.log.sh
+++ demo/work.log.sh
@@ -1,3 +1,3 @@
-/home/siyang/Bin/software_pip/YH_script/AsmVar/lastdb -c -m1111110 -v b36_1_1000000_3000000.lastdb b36_1_1000000_3000000.fa 
-/home/siyang/Bin/MyPipe/Assembly/develop/1.1.0/third_party/variantstudio/bin/lastal -e25 -v -q3 -j4 b36_1_1000000_3000000.lastdb query.fa | /home/siyang/Bin/MyPipe/Assembly/develop/1.1.0/third_party/variantstudio/bin/last-split -s35 -v > align.maf 2> align.log
-rm b36_1_1000000_3000000.lastdb.*
+/home/siyang/Bin/software_pip/YH_script/AsmVar/lastdb -c -m1111110 -v target.lastdb target.fa 
+/home/siyang/Bin/MyPipe/Assembly/develop/1.1.0/third_party/variantstudio/bin/lastal -e25 -v -q3 -j4 target.lastdb query.fa | /home/siyang/Bin/MyPipe/Assembly/develop/1.1.0/third_party/variantstudio/bin/last-split -s35 -v > align.maf 2> align.log
+rm target.lastdb.*
