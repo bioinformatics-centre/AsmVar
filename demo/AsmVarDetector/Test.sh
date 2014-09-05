df01d7dfc0e976a1251ff14cbd501f03f800d777
Shujia Huang <huangshujia9@gmail.com>
2014-09-05 11:52:48 +0800

    modfiy the demo case

---
+++ demo/AsmVarDetector/Test.sh
@@ -0,0 +1,3 @@
+#time ../src/AsmvarDetect/ASV_VariantDetector -i align.maf -t target.fa -q query.fa -s 1025 -o t4.1025 > t.age
+time ../../src/AsmvarDetect/ASV_VariantDetector -r Target4 -i ../Data/align.maf -t ../Data/target.fa -q ../Data/query.fa -s 1025 -o t4.1025 > t.age
+
